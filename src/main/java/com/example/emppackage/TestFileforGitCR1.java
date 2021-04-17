41e328c85e097e623e3c6d968a275126297e1857
Chandrapm786 <chandra.pm.786@gmail.com>
2021-04-17 11:37:14 +0530

    Change CR 1  for one file added - TestFileForGitCR1

---
+++ src/main/java/com/example/emppackage/TestFileforGitCR1.java
@@ -0,0 +1,10 @@
+package com.example.emppackage;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class TestFileforGitCR1 {
+
+    TestFileforGitCR1(){
+            System.out.println("New change CR for git files r ");
+    }
+}
